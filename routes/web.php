b6c14b3055dd87e6cc9971a9c2f903fafa72c892
emrealsandev <97472215+emrealsandev@users.noreply.github.com>
2022-09-11 22:54:38 +0300

    Laravel mail verify proccess and Mail view style changes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Auth::routes([
+//     'verify' => true
+// ]);
+
+Route::get('/email/verify', function () {
+    return view('auth.verify-email');
+})->middleware('auth')->name('verification.notice');
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+
+    return redirect('/dashboard');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth','verified'])->name('dashboard');
+
+require __DIR__.'/auth.php';
